8212bc926ad72d8540296e65814faec18993f9bc
assem-khaled <assem.khaled10@gmail.com>
2020-09-20 20:16:22 +0200

    Added Ports

--- 02-MCAL/01-RCC/RCC_interface.h
+++ 02-MCAL/01-RCC/RCC_interface.h
@@ -10,6 +10,11 @@
 #define	RCC_APB1	1
 #define	RCC_APB2	2
 
+/* PORTS */
+#define RCC_PORTA	2
+#define RCC_PORTB	3
+#define RCC_PORTC	4
+
 void RCC_voidInitSysClock(void);
 void RCC_voidEnableClock(u8 Copy_u8BusId, u8 Copy_u8PerId);
 void RCC_voidDisableClock(u8 Copy_u8BusId, u8 Copy_u8PerId);
