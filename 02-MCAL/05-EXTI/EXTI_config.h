1d01410b4b7e478ce7701c62b619b4b05ca87150
assem-khaled <assem.khaled10@gmail.com>
2020-09-25 18:34:33 +0200

    Added EXTI

---
+++ 02-MCAL/05-EXTI/EXTI_config.h
@@ -0,0 +1,23 @@
+/************************************************/
+/* Author    : Assem Khaled					*/
+/* Version   : V01							*/
+/* Date      : 22 Aug 2020						*/
+/************************************************/
+
+#ifndef EXTI_CONFIG_H
+#define EXTI_CONFIG_H
+
+
+/* Options:	MEXTI_LINE 0 : 15			*/
+
+#define EXTI_LINE	MEXTI_LINE0 /* 0 : 15 */
+
+/* Options:	MEXTI_RISING
+			MEXTI_FALLING
+			MEXTI_ON_CHANGE				*/
+			
+#define EXIT_SENSE_MODE		MEXTI_RISING
+
+
+
+#endif
