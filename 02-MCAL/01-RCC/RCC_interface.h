a42d19444ec05e77d91def320db2b92d6896ab56
assem-khaled <assem.khaled10@gmail.com>
2020-10-13 17:10:53 +0200

    Added MRCC_AHB_DMA1

--- 02-MCAL/01-RCC/RCC_interface.h
+++ 02-MCAL/01-RCC/RCC_interface.h
@@ -1,20 +1,24 @@
 /************************************************/
-/* Author    : Assem Khaled					*/
-/* Version   : V01							*/
+/* Author    : Assem Khaled						*/
+/* Version   : V02								*/
 /* Date      : 8 Aug 2020						*/
-/*************************************************/
+/************************************************/
 #ifndef _RCC_INTERFACE_H
 #define _RCC_INTERFACE_H
+
 
 #define MRCC_AHB	0
 #define	MRCC_APB1	1
 #define	MRCC_APB2	2
 
-/* PORTS */
-#define MRCC_AFIO	0
-#define MRCC_PORTA	2
-#define MRCC_PORTB	3
-#define MRCC_PORTC	4
+/* APB2 Clock Enable */
+#define MRCC_APB2_AFIO		0
+#define MRCC_APB2_PORTA		2
+#define MRCC_APB2_PORTB		3
+#define MRCC_APB2_PORTC		4
+/* AHB Clock Enable */
+#define MRCC_AHB_DMA1		0
+
 
 void MRCC_voidInitSysClock(void);
 void MRCC_voidEnableClock(u8 Copy_u8BusId, u8 Copy_u8PerId);
