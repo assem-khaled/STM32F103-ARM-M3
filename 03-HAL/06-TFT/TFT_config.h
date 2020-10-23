e7e3be5d6ba9b31fcacac734ac0eeece71679016
assem-khaled <assem.khaled10@gmail.com>
2020-10-23 16:25:07 +0200

    Version Update

--- 03-HAL/06-TFT/TFT_config.h
+++ 03-HAL/06-TFT/TFT_config.h
@@ -1,6 +1,6 @@
 /************************************************/
 /* Author    : Assem Khaled						*/
-/* Version   : V02								*/
+/* Version   : V03								*/
 /* Date      : 21 Sep 2020						*/
 /************************************************/
 #ifndef TFT_CONFIG_H
@@ -12,8 +12,11 @@
 #define TFT_RST_PIN  	GPIOA,1
 
 /* Write Max Dimensions */
-#define TFT_MAX_X		127
-#define TFT_MAX_Y		159
+#define TFT_MAX_X		128
+#define TFT_MAX_Y		160
+
+/* Space between each character */
+#define TFT_CHAR_SPACING		2
 
 
 #endif
--- 03-HAL/06-TFT/TFT_program.c
+++ 03-HAL/06-TFT/TFT_program.c
@@ -1,6 +1,6 @@
 /************************************************/
 /* Author    : Assem Khaled						*/
-/* Version   : V02								*/
+/* Version   : V03								*/
 /* Date      : 21 Sep 2020						*/
 /************************************************/
 #include "STD_TYPES.h"
@@ -50,32 +50,14 @@ void HTFT_voidInitialize(void)
 void HTFT_voidDisplayImage(const u16* Copy_u16Image)
 {
 	u16 Local_u16Counter;
-	u16 Local_u16Size = TFT_MAX_X * TFT_MAX_Y;
+	u16 Local_u16Size = (TFT_MAX_X- 1) * (TFT_MAX_Y - 1);
 
-	/* Set X Address */
-	voidWriteCommand(0x2A);
-	voidWriteData(0);
-	voidWriteData(0);
-	voidWriteData(0);
-	voidWriteData(TFT_MAX_X);
-	
-	/* Set Y Address */
-	voidWriteCommand(0x2B);
-	voidWriteData(0);
-	voidWriteData(0);
-	voidWriteData(0);
-	voidWriteData(TFT_MAX_Y);
-
-	/* RAM Write */
-	voidWriteCommand(0x2C);
+	/* Set Address */
+	voidSetAddress(0, (TFT_MAX_X- 1), 0, (TFT_MAX_Y - 1));
 
 	for(Local_u16Counter = 0; Local_u16Counter< Local_u16Size; Local_u16Counter++)
 	{
-		/* Write the high byte */
-		voidWriteData( (Copy_u16Image[Local_u16Counter] >> 8) );
-
-		/* Write the low byte */
-		voidWriteData((Copy_u16Image[Local_u16Counter] & 0x00ff));
+		voidSetColour(Copy_u16Image[Local_u16Counter]);
 	}
 }
 
@@ -83,65 +65,106 @@ void HTFT_voidFillColor(u16 Copy_u16Color)
 {
 	u16 Local_u16Counter;
 
-	/* Set X Address */
-	voidWriteCommand(0x2A);
-	voidWriteData(0);
-	voidWriteData(0);
-	voidWriteData(0);
-	voidWriteData(127);
-
-	/* Set Y Address */
-	voidWriteCommand(0x2B);
-	voidWriteData(0);
-	voidWriteData(0);
-	voidWriteData(0);
-	voidWriteData(159);
-
-	/* RAM Write */
-	voidWriteCommand(0x2C);
+	/* Set Address */
+	voidSetAddress(0, (TFT_MAX_X- 1), 0, (TFT_MAX_Y - 1));
 
 	for(Local_u16Counter = 0; Local_u16Counter< 20480; Local_u16Counter++)
 	{
-		/* Write the high byte */
-		voidWriteData( (Copy_u16Color >> 8) );
-
-		/* Write the low byte */
-		voidWriteData((Copy_u16Color & 0x00ff));
+		voidSetColour(Copy_u16Color);
 	}
 }
 
-void HTFT_voidDrawRect(u8 Copy_u8x1, u8 Copy_u8x2, u8 Copy_u8y1, u8 Copy_u8y2, u16 Copy_u16Color)
+void HTFT_voidDrawRect(u8 Copy_u8x1, u8 Copy_u8w, u8 Copy_u8y1, u8 Copy_u8h, u16 Copy_u16Color)
 {
 	u16 Local_u16Counter;
+	u8 Copy_u8x2 = Copy_u8x1 + Copy_u8w;
+	u8 Copy_u8y2 = Copy_u8y1 + Copy_u8h;
 	u16 Local_u16Size = (Copy_u8x2 - Copy_u8x1) * (Copy_u8y2 - Copy_u8y1);
 
-	/* Set X Address */
-	voidWriteCommand(0x2A);
-	voidWriteData(0);
-	voidWriteData(Copy_u8x1);
-	voidWriteData(0);
-	voidWriteData(Copy_u8x2);
-
-	/* Set Y Address */
-	voidWriteCommand(0x2B);
-	voidWriteData(0);
-	voidWriteData(Copy_u8y1);
-	voidWriteData(0);
-	voidWriteData(Copy_u8y2);
-
-	/* RAM Write */
-	voidWriteCommand(0x2C);
+	/* Set Address */
+	voidSetAddress(Copy_u8x1, Copy_u8x2, Copy_u8y1, Copy_u8y2);
 
 	for(Local_u16Counter = 0; Local_u16Counter< Local_u16Size; Local_u16Counter++)
 	{
-		/* Write the high byte */
-		voidWriteData( (Copy_u16Color >> 8) );
-
-		/* Write the low byte */
-		voidWriteData((Copy_u16Color & 0x00ff));
+		voidSetColour(Copy_u16Color);
 	}
 }
 
+void HTFT_voidDrawChar(u8 Copy_u8Char, u8 Copy_u8x1, u8 Copy_u8y1, f32 Copy_f32Size, u16 Copy_u16Color, u16 Copy_u16BackColor)
+{
+	/* Get array index */
+	u8 local_u8CharIndex = 0 ;
+	if (( Copy_u8Char >= ' ' ))
+	{
+		local_u8CharIndex = Copy_u8Char - 32 ;
+	}
+
+	// Draw the Background Color
+	HTFT_voidDrawRect(Copy_u8x1-((TFT_CHAR_WIDTH-1)*Copy_f32Size), ((TFT_CHAR_WIDTH-1)*Copy_f32Size)+ Copy_f32Size, Copy_u8y1-((TFT_CHAR_HIGHT-1)*Copy_f32Size)+ Copy_f32Size, ((TFT_CHAR_HIGHT-1)*Copy_f32Size)+ Copy_f32Size, Copy_u16BackColor);
+
+	for(u8 x = 0; x < TFT_CHAR_WIDTH; x++)
+	{
+		for(u8 y = 0; y < TFT_CHAR_HIGHT; y++)
+		{
+			if( (TFT_Font5x7[(local_u8CharIndex*5) + x]) & (1 << y))
+			{
+				HTFT_voidDrawRect(Copy_u8x1-(x*Copy_f32Size), (u8)Copy_f32Size, Copy_u8y1-(y*Copy_f32Size), (u8)Copy_f32Size, Copy_u16Color);
+			}
+		}
+	}
+}
+
+void HTFT_voidDrawText(u8 *Copy_u8Text, s8 Copy_s8x1, u8 Copy_u8y1, f32 Copy_f32Size, u16 Copy_u16Color, u16 Copy_u16BackColor)
+{
+	s8 Local_s8x1 = Copy_s8x1;
+	u8 Local_u8y1 = Copy_u8y1;
+
+	Local_s8x1 -= Copy_f32Size;
+	Local_u8y1 -= Copy_f32Size-1;
+
+	while(*Copy_u8Text)
+	{
+		if (Local_s8x1 <= 0) // return to the next line
+		{
+			Local_s8x1 = Copy_s8x1;
+			Local_u8y1 =  Copy_u8y1 - (TFT_CHAR_HIGHT * Copy_f32Size) - TFT_CHAR_SPACING;
+		}
+
+		HTFT_voidDrawChar(*Copy_u8Text, Local_s8x1, Local_u8y1, Copy_f32Size, Copy_u16Color, Copy_u16BackColor); // Draw char
+
+		Copy_u8Text++; // Next char
+
+		Local_s8x1 -= (TFT_CHAR_WIDTH * Copy_f32Size) + TFT_CHAR_SPACING; // Next Char Location in TFT
+	}
+}
+
+void HTFT_voidDrawNum(u32 u32Num, u8 Copy_u8x1, u8 Copy_u8y1, f32 Copy_f32Size, u16 Copy_u16Color, u16 Copy_u16BackColor)
+{
+	u8  i , j , temp , num[10]={0} ;
+
+	if (u32Num == 0)
+	{
+		HTFT_voidDrawChar('0', Copy_u8x1, Copy_u8y1, Copy_f32Size, Copy_u16Color, Copy_u16BackColor);
+		return;
+	}
+
+	for(i=0;u32Num>0;i++)
+		{
+			num[i]=u32Num%10 + 0x30;
+			u32Num=u32Num/10;
+		}
+	for(j=0,i--;j<i;j++,i--)
+		{
+			temp=num[i];
+			num[i]=num[j];
+			num[j]=temp;
+		}
+	HTFT_voidDrawText(num, Copy_u8x1, Copy_u8y1, Copy_f32Size, Copy_u16Color, Copy_u16BackColor);
+}
+
+
+
+/////////////////* Private Functions */////////////////
 
 static void voidWriteCommand(u8 Copy_u8Command)
 {
@@ -164,3 +187,33 @@ static void voidWriteData(u8 Copy_u8Data)
 	/* Send data over SPI */
 	MSPI_voidSendReceiveSynch(Copy_u8Data, &Local_u8Temp);
 }
+
+static void voidSetAddress(u8 Copy_u8x1, u8 Copy_u8x2, u8 Copy_u8y1, u8 Copy_u8y2)
+{
+	/* Set X Address */
+	voidWriteCommand(0x2A);
+	voidWriteData(0);
+	voidWriteData(Copy_u8x1);
+	voidWriteData(0);
+	voidWriteData(Copy_u8x2);
+
+	/* Set Y Address */
+	voidWriteCommand(0x2B);
+	voidWriteData(0);
+	voidWriteData(Copy_u8y1);
+	voidWriteData(0);
+	voidWriteData(Copy_u8y2);
+
+	/* RAM Write */
+	voidWriteCommand(0x2C);
+}
+
+static void voidSetColour(u16 Copy_u16Color)
+{
+	/* Write the high byte */
+	voidWriteData( (Copy_u16Color >> 8) );
+
+	/* Write the low byte */
+	voidWriteData((Copy_u16Color & 0x00ff));
+}
+
